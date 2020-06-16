630c7331cb62fc76ef700efd8c82d0857a9391c8
calvinoea <calvinebunamu@gmail.com>
2020-06-16 11:26:32 +0100

    Changes made in view and user controller for form functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,22 @@ class UsersController < ApplicationController
   def new
     # @user = User.new
   end
-  
+
   def create
-    # @user = User.create(user_params)
+  @user = User.new(user_params)
+
+  # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+  if @user.save
+    redirect_to new_users_path
+  else
+    render :new
   end
 
-  # private
-  # def user_params
-  #   params.require(:user).permit(:username, :email, :password)
-  # end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
