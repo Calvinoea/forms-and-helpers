034d75e18ed3a001fd4c8ad71a618e1a143ac521
calvinoea <calvinebunamu@gmail.com>
2020-06-17 15:18:59 +0100

    Changes made to views and routes after TSE review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     if @user.save
-      redirect_to new_users_path
+      redirect_to new_user_path
     else
       render :new
     end
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to new_user_path
+      redirect_to edit_user_path
     else
       render :edit
     end
